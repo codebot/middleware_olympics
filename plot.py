690357749099790fa3ec9a25aed9a942f08da53a
Morgan Quigley <morgan@osrfoundation.org>
2020-07-17 09:35:20 +0800

    add ros1 plot with/without nagle

--- plot.py
+++ plot.py
@@ -5,11 +5,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 if __name__=='__main__':
-    ros1 = np.genfromtxt('ros1.csv', delimiter=',')
+    ros1_nagle = np.genfromtxt('ros1_False.csv', delimiter=',')
+    ros1_tcp_nodelay = np.genfromtxt('ros1_True.csv', delimiter=',')
     cyclone = np.genfromtxt('cyclone.csv', delimiter=',')
     fastrtps = np.genfromtxt('fastrtps.csv', delimiter=',')
 
-    plt.loglog(ros1[:, 0], ros1[:, 2], marker='o', markersize=5)
+    plt.loglog(ros1_nagle[:, 0], ros1_nagle[:, 2], marker='o', markersize=5)
+    plt.loglog(ros1_tcp_nodelay[:, 0], ros1_tcp_nodelay[:, 2], marker='o', markersize=5)
     plt.loglog(fastrtps[:, 0], fastrtps[:, 2], marker='o', markersize=5)
     plt.loglog(cyclone[:, 0], cyclone[:, 2], marker='o', markersize=5)
 
@@ -18,7 +20,7 @@ if __name__=='__main__':
     plt.xlabel('Message payload size (bytes)')
 
     plt.legend(
-      ('ROS 1', 'FastRTPS', 'Cyclone'),
+      ('ROS 1 (default)', 'ROS 1 (TCP_NODELAY)', 'FastRTPS', 'Cyclone'),
       loc='upper left')
 
     plt.show()
