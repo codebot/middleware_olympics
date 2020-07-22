c01a373ea03ae6029337f31098c306ee1f7d472c
Morgan Quigley <morgan@osrfoundation.org>
2020-07-22 10:04:57 +0800

    compile one thing at a time

--- compile.sh
+++ compile.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 set -o errexit
 
-echo "Building the ROS 1 contestant..."
-mkdir -p build/ros1/src
-ln -sf `pwd`/ros1/ros1_contestant build/ros1/src
-pushd build/ros1
-source /opt/ros/melodic/setup.bash
-catkin build
-popd
+if [ $# -lt 1 ]; then
+  echo "usage: compile.sh MIDDLEWARE_NAME"
+  echo "  example: compile.sh ros1"
+  exit
+fi
 
+MIDDLEWARE_NAME=$1
 
-echo "Building the ROS 2 contestant..."
-mkdir -p build/ros2/src
-ln -sf `pwd`/ros2/ros2_contestant build/ros2/src
-pushd build/ros2
-source /opt/ros/eloquent/setup.bash
-colcon build
-popd
+if [ $MIDDLEWARE_NAME = ros1 ]; then
+  echo "Building the ROS 1 contestants..."
+  mkdir -p build/ros1/src
+  ln -sf `pwd`/ros1/ros1_contestant build/ros1/src
+  cd build/ros1
+  source /opt/ros/melodic/setup.bash
+  catkin build
+elif [ $MIDDLEWARE_NAME = ros2 ]; then
+  echo "Building the ROS 2 contestants...."
+  mkdir -p build/ros2/src
+  ln -sf `pwd`/ros2/ros2_contestant build/ros2/src
+  cd build/ros2
+  source /opt/ros/eloquent/setup.bash
+  colcon build
+elif [ $MIDDLEWARE_NAME = zenoh ]; then
+  echo "Building the Zenoh contestants..."
+else
+  echo "unknown middleware: $MIDDLEWARE_NAME"
+fi
